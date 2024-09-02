32adbc94775c80f0a8fba957bfdcd03ae8f0ccd6
Traveller Coder <guillermobukowski@gmail.com>
2024-09-02 17:23:25 -0300

    finalizacion de consumo de api

--- script.js
+++ script.js
@@ -12,10 +12,60 @@ document.getElementById('searchButton').addEventListener('click', () => {
         alert('Ingresa una ciudad')
     }
 })
+
+
 //con esta funcion llamamos a la api para que devuelva la info pedida
 function fetchWeather(city){
     fetch(`${urlBase}?q=${city}&appid=${API_KEY}`)
     .then( data => data.json())
-    .then(data => console.log(data));
-    
+    .then( data => showWeatherData(data))
+    .then( data => (data))
 }
+
+function showWeatherData(data) {
+    const divResponseData = document.getElementById('responseData');
+    divResponseData.innerHTML = '';
+
+    const cityName = data.name;
+    const weather = data.weather[0].description;
+    const weatherIcon = data.weather[0].icon;
+    const temp = data.main.temp;
+    const feelsLike = data.main.feels_like;
+    const minTemp = data.main.temp_min;
+    const maxtemp = data.main.temp_max;
+    const wind = data.wind.speed;
+    const sunset = data.sys.sunset;
+    const sunrise = data.sys.sunrise;
+
+    const cityInfo = document.createElement('h2');
+    cityInfo.textContent = `${cityName}`
+
+    const iconInfo = document.createElement('img');
+    iconInfo.src = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png` 
+
+    const tempInfo = document.createElement('p');
+    tempInfo.textContent = `Temperatura Actual: ${Math.floor(temp - diffKelvin)}°C`;
+
+    const feelsLikeInfo = document.createElement('p');
+    feelsLikeInfo.textContent = `Sensacion Termica: ${Math.floor(feelsLike - diffKelvin)}°C`;
+
+    const paramsInfo = document.createElement('p');
+    paramsInfo.textContent = `Minima: ${Math.floor(minTemp - diffKelvin)}°C    Maxima: ${Math.floor(maxtemp - diffKelvin)}°C`
+
+    const windInfo = document.createElement('p');
+    windInfo.textContent = `Viento: ${wind} KM/H`;
+
+    const sunInfo = document.createElement('p');
+    sunInfo.textContent = `Sunset: ${sunset} Sunrise: ${sunrise}`
+
+
+
+    divResponseData.appendChild(cityInfo);
+    divResponseData.appendChild(iconInfo);
+    divResponseData.appendChild(tempInfo);
+    divResponseData.appendChild(feelsLikeInfo);
+    divResponseData.appendChild(paramsInfo);
+    divResponseData.appendChild(windInfo);
+    divResponseData.appendChild(sunInfo);
+}
+
